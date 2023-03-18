b131b11ae6ebafc8bd70bc1e0bd213623a10acac
Jose Vega <estib@amazon.de>
2023-03-18 12:46:14 +0100

    Add global variable declaration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
+type Global = typeof globalThis & {
+    prisma: PrismaClient | undefined;
+};
+
+declare const global: Global;
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const prisma: PrismaClient = isProd
